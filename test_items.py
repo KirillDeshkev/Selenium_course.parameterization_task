2c1e151c33760f76259f0dd4565f631cbdea8104
Kirill Deshkevich <kirill.deshkev@gmail.com>
2019-07-01 17:49:52 +0300

    Add validation error messages

--- test_items.py
+++ test_items.py
@@ -7,5 +7,5 @@ def test_add_to_basket_button_is_present(browser):
     browser.get(link)
     add_button = browser.find_element_by_class_name("btn-add-to-basket")
     # time.sleep(30)
-    assert add_button.is_displayed()
-    assert add_button.is_enabled()
+    assert add_button.is_displayed(), "Button is not displayed"
+    assert add_button.is_enabled(), "Button is disabled"
